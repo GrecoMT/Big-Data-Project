1e38ce8e366851bbcf2624c85df7199cbbe094ed
vpresta9@gmail.com <vpresta9@gmail.com>
2025-02-07 12:26:07 +0100

    removed unused function get_season(month)

--- utils.py
+++ utils.py
@@ -75,16 +75,6 @@ def get_lng(address):
     _, lng = get_coordinates_osm(address)
     return lng
 
-def get_season(month):
-    if month in [12, 1, 2]:
-        return "Winter"
-    elif month in [3, 4, 5]:
-        return "Spring"
-    elif month in [6, 7, 8]:
-        return "Summer"
-    else:
-        return "Autumn"
-
 def graficoTrend(dataframe, single : bool):
     trend_pandas = dataframe.toPandas()
     # Impostare stile del grafico
